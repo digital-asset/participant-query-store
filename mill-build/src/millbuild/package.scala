9c45fdba59d4716e61edf8da2955d0b19e9aa397
Adrien Piquerez <adrien.piquerez@digitalasset.com>
2026-09-16 10:11:53 +0200

    Bump Daml

--- mill-build/src/millbuild/package.scala
+++ mill-build/src/millbuild/package.scala
@@ -14,7 +14,7 @@ package object millbuild {
 
     // Latest snapshot: https://console.cloud.google.com/artifacts/docker/da-images/europe/public-unstable/components%2Fdamlc
     // Latest stable: https://console.cloud.google.com/artifacts/docker/da-images/europe/public/components%2Fdamlc
-    val damlc = "3.6.0-snapshot.20260828.14793.0.vfa06ab6b"
+    val damlc = "3.7.0-snapshot.20260915.14814.0.vf3b4b347"
 
     // Latest snapshot: https://console.cloud.google.com/artifacts/docker/da-images/europe/public-unstable/components%2Fcanton-open-source
     // Latest stable: https://console.cloud.google.com/artifacts/docker/da-images/europe/public/components%2Fcanton-open-source
