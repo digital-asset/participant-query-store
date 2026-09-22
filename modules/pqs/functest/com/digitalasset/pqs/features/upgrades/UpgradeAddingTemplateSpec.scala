d3866a85e2a0d65feb3150f6103bad5ba63e7342
Adrien Piquerez <adrien.piquerez@digitalasset.com>
2026-09-22 16:56:10 +0200

    Fix compilation

--- modules/pqs/functest/com/digitalasset/pqs/features/upgrades/UpgradeAddingTemplateSpec.scala
+++ modules/pqs/functest/com/digitalasset/pqs/features/upgrades/UpgradeAddingTemplateSpec.scala
@@ -7,7 +7,6 @@ import com.digitalasset.pqs.SharedLedgerAndPostgresTest
 import com.digitalasset.pqs.functest.matchers.*
 import com.digitalasset.pqs.functest.table.*
 import com.digitalasset.pqs.services.daml.*
-import com.digitalasset.pqs.services.daml.DamlSdk.onlyDamlLfVersion
 import com.digitalasset.pqs.services.postgres.Database.__contracts
 import com.digitalasset.pqs.services.postgres.Postgres
 import com.digitalasset.pqs.services.pqs.Pqs
