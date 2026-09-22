7f45157a2ea57f95768fc1b3ceec8a2abaa9ab9c
Adrien Piquerez <adrien.piquerez@digitalasset.com>
2026-09-22 08:24:55 +0200

    Bump Daml and Canton 3.6

--- mill-build/src/millbuild/package.scala
+++ mill-build/src/millbuild/package.scala
@@ -14,11 +14,11 @@ package object millbuild {
 
     // Latest snapshot: https://console.cloud.google.com/artifacts/docker/da-images/europe/public-unstable/components%2Fdamlc
     // Latest stable: https://console.cloud.google.com/artifacts/docker/da-images/europe/public/components%2Fdamlc
-    val damlc = "3.6.0-snapshot.20260828.14793.0.vfa06ab6b"
+    val damlc = "3.6.0-snapshot.20260915.14804.0.v53478765"
 
     // Latest snapshot: https://console.cloud.google.com/artifacts/docker/da-images/europe/public-unstable/components%2Fcanton-open-source
     // Latest stable: https://console.cloud.google.com/artifacts/docker/da-images/europe/public/components%2Fcanton-open-source
-    val canton = "3.6.0-snapshot.20260818.20026.0.v41046c3b"
+    val canton = "3.6.0-snapshot.20260921.20299.0.v50e85c6a"
 
     val dockerClient = "3.4.0"
     val flyway = "13.4.0"
